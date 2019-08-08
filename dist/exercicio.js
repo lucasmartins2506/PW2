3e635e1b1297895e62a679c2aeae931186fd1ca9
lucasmartins2506 <lucasnalua25@gmail.com>
2019-08-07 22:05:19 -0300

    Exercicio

---
+++ dist/exercicio.js
@@ -0,0 +1,13 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 5;
+var media = 0;
+media = nota1 + nota2 + nota3;
+media = media / 3;
+console.log("A média é " + media);
+if (media >= 7) {
+    console.log("aprovado");
+}
+else {
+    console.log("reprovado");
+}
---
+++ exercicio.ts
@@ -0,0 +1,20 @@
+let nota1:number = 10
+let nota2:number = 9
+let nota3:number = 5
+let media:number = 0
+
+media = nota1 + nota2 + nota3
+media = media/3
+
+console.log("A média é " + media)
+
+if (media >= 7)
+{
+    console.log("aprovado")
+}
+else
+{
+    console.log("reprovado")
+}
+
+   
